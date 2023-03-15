1e4b59dc35818f9df41a77e07a0c556a3ad916fa
lyk082401 <lyk082401@qq.com>
2023-03-15 19:44:07 +0800

    更新代码

--- DownloadRemoteFile.sh
+++ DownloadRemoteFile.sh
@@ -15,6 +15,10 @@ echo "###" && echo "### 安装依赖包 ###" && echo "###"
 type curl || sudo apt-get install curl -y
 type wget || sudo apt-get install wget -y
 
+echo "###" && echo "### 代码块 ###" && echo "###"
+curl -sSLO "https://github.com/lyk082401/lyk082401/archive/refs/heads/main.zip"
+mv main.zip artifacts/
+
 echo "###" && echo "### 环境变量 export ###" && echo "###"
 export
 
@@ -24,9 +28,6 @@ env
 echo "###" && echo "### 环境变量 set ###" && echo "###"
 set
 
-echo "###" && echo "### 代码块 ###" && echo "###"
-curl -sSLO "https://github.com/lyk082401/lyk082401/archive/refs/heads/main.zip"
-mv main.zip artifacts/
 exit 0
 
 # 关闭sendmail的服务
